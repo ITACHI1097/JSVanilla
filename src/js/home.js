e5c8abbe11437e5d57e73d34674dbcd1bfcbc0b5
sangeeky <sangeeky@outlook.com>
2020-07-09 21:58:26 -0500

    Promesas

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,42 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUser = new Promise(function(todoBien, todoMal) {
+  // llamada a un api en cierto tiempo
+  setTimeout(function() {
+    ///luego de 3 segundos se ejecuta
+    todoBien('Se ejecuto perfecto Unito');
+  }, 3000)
+});
+
+const getUserAll = new Promise(function(todoBien, todoMal) {
+  // llamada a un api en cierto tiempo
+  setTimeout(function() {
+    ///luego de 3 segundos se ejecuta
+    todoBien('Se ejecuto perfecto TODOS');
+  }, 5000)
+});
+
+// getUser
+//   .then(function(){
+//     console.log('todo esta bien')
+//   })
+//   .catch(function(Message){
+//     console.log(Message);
+//   })
+///error en esta promesa
+
+//Promise.all
+Promise.race([
+  getUser,
+  getUserAll,
+])
+.then(function(Message) {
+  console.log(Message)
+})
+.catch(function(Message) {
+  console.log(Message)
+})
+
+// error en todas las promesas
